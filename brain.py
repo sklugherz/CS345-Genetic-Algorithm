8ae4a3afc6323f17c4e21bee372ac9e2c83b670a
Stephen Klugherz <klugherz@colostate.edu>
2024-05-02 14:40:23 -0600

    comeneted out unsused imports

--- brain.py
+++ brain.py
@@ -1,7 +1,7 @@
-import numpy as np
-import tensorflow as tf
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Input
+#import numpy as np
+#import tensorflow as tf
+from keras.models import Sequential #, Model
+from keras.layers import Dense #, Dropout, Input
 from keras.optimizers import SGD
 
 
