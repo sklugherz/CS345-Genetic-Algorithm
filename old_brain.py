127f8ec2b77da4b9c3932c64da0594e4918b6ffc
Stephen Klugherz <klugherz@colostate.edu>
2024-05-02 18:44:16 -0600

    barin

--- old_brain.py
+++ old_brain.py
@@ -5,4 +5,13 @@ from keras.layers import Dense, Dropout, Input
 from keras.optimizers import Adam
 
 class brain:
-    pass
+    def __init__():
+        nn = tf.keras.Sequential()
+        nn.add(tf.keras.layers.Dense(8, activation='relu', input_shape=(5,)))
+        # nn.add(tf.keras.layers.Dense(8, activation='relu'))
+        # nn.add(tf.keras.layers.Dense(8, activation='relu')) #i think the first layer is all we need
+        nn.add(tf.keras.layers.Dense(1)) #output layer
+                         #could be 2, acitvation='softmax'
+
+        opt = tf.keras.optimizers.Adam() #can play with this
+        nn.compile(loss='mse', optimizer=opt, metrics=['accuracy', 'mse'])
